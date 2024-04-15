5c2011fe0bac7a77eb793a5fa62ff2b16ef19e6e
Yashaswi Tiwari <92193824+yashaswi29@users.noreply.github.com>
2024-04-16 03:39:17 +0530

    Update server.js #3

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 123456;
+const PORT = process.env.PORT || 12345;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
